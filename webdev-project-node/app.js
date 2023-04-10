1d03215bfb0f51b45bd7eb7527b18127c6a99f33
dheekksha <116768851+dheekkshaR@users.noreply.github.com>
2023-04-10 19:41:11 -0400

    scratch

--- webdev-project-node/app.js
+++ webdev-project-node/app.js
@@ -1,5 +1,7 @@
 
 const express = require('express')
 const app = express()
+app.get('', (req, res) => {res.send('Hello first!')})
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.listen(4000)
+// console.log('Hello World!');
